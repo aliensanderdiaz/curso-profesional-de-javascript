fd8f141663a9f778e8418a84bbf743659f59579b
aliensanderdiaz <aliensanderdiaz@gmail.com>
2022-07-07 02:03:36 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# CURSO PROFESIONAL DE JAVASCRIPT PLATZI
+
+## SECCIÓN 1 DE 42 - ¿Qué significa ser un profesional de Javascript?
+
+## SECCIÓN 2 DE 42 - Aspectos que destacan a un profesional
+
+## SECCIÓN 3 DE 42 - Inicio del proyecto
+
+## SECCIÓN 4 DE 42 - Cómo llega un script al navegador
+
+## SECCIÓN 5 DE 42 - Scope
+
+## SECCIÓN 6 DE 42 - Closures
+
+## SECCIÓN 7 DE 42 - El primer plugin
+
+## SECCIÓN 8 DE 42 - this
+
+
---
+++ assets/index.js
@@ -0,0 +1,13 @@
+import MediaPlayer from './MediaPlayer.js'
+import AutoPlay from './plugins/AutoPlay.js'
+
+const video = document.querySelector('video')
+const playButton = document.querySelector('#playButton')
+const muteButton = document.querySelector('#muteButton')
+
+
+
+const player = new MediaPlayer({ el: video, plugins: [ new AutoPlay() ] })
+
+playButton.onclick = () => player.play()
+muteButton.onclick = () => player.muteunmute()
